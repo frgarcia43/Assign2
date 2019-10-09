ab9084e2dec0c5dac58bdaa5810a4070a5834fa8
Fermin Garcia Martinez <frgarcia43@gmail.com>
2019-10-08 18:23:09 -0700

    Initial commit of project.

---
+++ assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,68 @@
+/*
+ * Author: Fermin Garcia
+ * Class ID: 70641
+ * Assignment #: 2
+ * Description: This is the AddingMachine class. It has a
+ * constructor to create a string to hold the history
+ * and an int to hold the total
+ */
+
+package cse360assign2;
+
+public class AddingMachine 
+{
+
+	private int total;
+	
+	/**
+	 * Constructor that sets total to initial value
+	 */
+	public AddingMachine () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * The total of the calculations is returned
+	 * @return Int
+	 */
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/**
+	 * Adds the entered value to the total
+	 * @param value
+	 */
+	public void add (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Subtracts the entered value from the the total
+	 * @param value
+	 */
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/**
+	 * Returns the history of the calculations
+	 */
+	public String toString () 
+	{
+		return "";
+	}
+
+	/**
+	 * Resets the value of total to 0
+	 * and clears the history
+	 */
+	public void clear() 
+	{
+	
+	}
+}
